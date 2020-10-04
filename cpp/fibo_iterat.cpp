cdfeff7ab74e92b2145af8de8288ac488bdb73d9
anurag341 <55197365+anurag341@users.noreply.github.com>
2020-10-04 19:54:25 +0530

    added fibo_iterat.cpp

---
+++ cpp/fibo_iterat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void fib(int num) {
+   int x = 0, y = 1, z = 0;
+   for (int i = 0; i < num; i++) {
+      cout << x << " ";
+      z = x + y;
+      x = y;
+      y = z;
+   }
+}
+int main() {
+   int num;
+   cout << "Enter the number : ";
+   cin >> num;
+   cout << "\nThe fibonacci series : " ;
+   fib(num);
+   return 0;
+}
