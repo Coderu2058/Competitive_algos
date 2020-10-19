6a093e6caa6d8c922a1db0d47824eb158b42c469
Jahanvi Sharma <56484763+jahanvisharma-dotcom@users.noreply.github.com>
2020-10-19 23:09:10 +0530

    Update IntegerAdd.cpp

--- cpp/IntegerAdd.cpp
+++ cpp/IntegerAdd.cpp
@@ -1,1 +1,18 @@
-//enter code
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int firstNumber, secondNumber, sumOfTwoNumbers;
+    
+    cout << "Enter two integers: ";
+    cin >> firstNumber >> secondNumber;
+
+    
+    sumOfTwoNumbers = firstNumber + secondNumber;
+
+   
+    cout << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
+
+    return 0;
+}
